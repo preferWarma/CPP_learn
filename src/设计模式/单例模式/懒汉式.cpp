4f5d1370ee2d56861d42afd713aa366138b6200d
perferWarma <2961884371@qq.com>
2022-12-24 11:25:19 +0800

    使用delete关键字修改

--- src/设计模式/单例模式/懒汉式.cpp
+++ src/设计模式/单例模式/懒汉式.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 
-class Singleton
-{
+class Singleton {
 public:
 	// 获取单例实例对象
-	static Singleton &GetInstance()
-	{
+	static Singleton& GetInstance() {
 		// 利用局部静态变量实现单例
 		static Singleton instance;
 		return instance;
 	}
 
 	// 打印单例的地址
-	void print()
-	{
+	void print() {
 		std::cout << this << std::endl;
 	}
+
+	// 禁止外部拷贝或赋值
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
 
 private:
 	// 禁止外部构造和析构
 	Singleton() = default;
 	~Singleton() = default;
-
-	// 禁止外部拷贝或赋值
-	Singleton(const Singleton &) = default;
-	Singleton &operator=(const Singleton &);
 };
 
-int main()
-{
-	Singleton::GetInstance().print();
+int main() {
+	for (int i = 0; i < 10; i++) {
+		Singleton::GetInstance().print();
+	}
 	return 0;
 }
--- src/设计模式/单例模式/饿汉式.cpp
+++ src/设计模式/单例模式/饿汉式.cpp
@@ -1,48 +1,44 @@
 #include <iostream>
 #include <thread>
 
-class Singleton
-{
+class Singleton {
 public:
 	// 获取单例
-	static Singleton *GetInstance()
-	{
+	static Singleton* GetInstance() {
 		return pInstance;
 	}
 	// 释放单例, 进程退出时调用
-	static void DeleteInstance()
-	{
-		if (pInstance != nullptr)
-		{
+	static void DeleteInstance() {
+		if (pInstance != nullptr) {
 			delete pInstance;
 			pInstance = nullptr;
 		}
 	}
 
 	//打印单例的地址
-	void print()
-	{
+	void print() {
 		std::cout << this << std::endl;
 	}
+
+	// 禁止拷贝或赋值(使用delete关键字)
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
 
 private:
 	// 禁止外部构造和析构
 	Singleton() = default;
 	~Singleton() = default;
 
-	// 禁止外部拷贝或赋值
-	Singleton(const Singleton &) = default;
-	Singleton &operator=(const Singleton &);
-
 	// 唯一单例对象指针
-	static Singleton *pInstance;
+	static Singleton* pInstance;
 };
 
 // 代码一运行就初始化实例, 本身就线程安全
-Singleton *Singleton::pInstance = new (std::nothrow) Singleton;
+Singleton* Singleton::pInstance = new (std::nothrow) Singleton;
 
-int main()
-{
-	Singleton::GetInstance()->print();
+int main() {
+	for (int i = 0; i < 10; i++) {
+		Singleton::GetInstance()->print();
+	}
 	return 0;
 }
