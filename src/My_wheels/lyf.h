3ad2cd5097a78ae3a196f30ad3d8214505eeb12c
perferWarma <2961884371@qq.com>
2023-01-29 20:53:04 +0800

    优化可读性，增加print函数

--- src/My_wheels/lyf.h
+++ src/My_wheels/lyf.h
@@ -1,12 +1,14 @@
 #pragma once	// 防止重复包含
 
-#include <typeinfo>
 #include <cxxabi.h>
 #include <iostream>
 #include <string>
-#include <cstring>
 #include <vector>
 #include <sstream>
+
+using std::cout, std::cin, std::endl;
+using std::string, std::vector;
+using std::stringstream;
 
 # define Get_Type(x) lyf::type_class<decltype(x)>::get()	// 用于获取变量类型的快捷调用宏
 
@@ -21,11 +23,11 @@ namespace lyf {
 	class type_class {
 	public:
 		// 获取变量类型(依赖于typeid关键字)
-		static std::string get() {
-			std::string all_realName = std::string{abi::__cxa_demangle(typeid(cvr_saver<T>).name(), nullptr, nullptr, nullptr)};	// 包含cuv_saver结构体的全名
-			int pos1 = all_realName.find_first_of('<') + 1;	// 第一个'<'后的位置
-			int pos2 = all_realName.find_last_of('>');	// 最后一个'>'的位置
-			std::string realName = all_realName.substr(pos1, pos2 - pos1);	// 去掉干扰信息
+		static string get() {
+			string all_realName = string{abi::__cxa_demangle(typeid(cvr_saver<T>).name(), nullptr, nullptr, nullptr)};	// 包含cuv_saver结构体的全名
+			auto pos1 = all_realName.find_first_of('<') + 1;	// 第一个'<'后的位置
+			auto pos2 = all_realName.find_last_of('>');	// 最后一个'>'的位置
+			string realName = all_realName.substr(pos1, pos2 - pos1);	// 去掉干扰信息
 			return realName;
 		}
 	};
@@ -34,10 +36,10 @@ namespace lyf {
 	/// @param str 要分隔的字符串
 	/// @param delim 分隔符
 	/// @return 分隔后的字符串数组, 以vector<string>形式返回
-	std::vector<std::string> split(const std::string& str, const char delim) {
-		std::stringstream ss(str);
-		std::string s;
-		std::vector<std::string> res;
+	vector<string> split(const string& str, const char delim) {
+		stringstream ss(str);
+		string s;
+		vector<string> res;
 		res.clear();
 		while (getline(ss, s, delim)) {
 			res.push_back(s);
@@ -49,12 +51,12 @@ namespace lyf {
 	/// @param str 要分隔的字符串
 	/// @param delim 分隔符
 	/// @return 分隔后的字符串数组, 以vector<string>形式返回
-	std::vector<std::string> split(const std::string& str, const std::string& delim) {
-		std::string::size_type pos1, pos2;
+	vector<string> split(const string& str, const string& delim) {
+		string::size_type pos1, pos2;
 		pos1 = 0;
 		pos2 = str.find_first_of(delim, pos1);	// 查找第一个分隔符的位置
-		std::vector<std::string> res;
-		while (std::string::npos != pos2) {
+		vector<string> res;
+		while (string::npos != pos2) {
 			res.push_back(str.substr(pos1, pos2 - pos1));
 			pos1 = pos2 + delim.size();
 			pos2 = str.find(delim, pos1);
@@ -62,6 +64,18 @@ namespace lyf {
 		if (pos1 != str.size())
 			res.push_back(str.substr(pos1));
 		return res;
+	}
+
+	/// @brief 形参包遍历打印元素(C++17以后)
+	template<typename T, typename ...Args>
+	void print(T&& v, Args&&... args) {
+		cout << v << " ";
+		if constexpr (sizeof...(args) > 0) {
+			print(std::forward<Args>(args)...);
+		}
+		else {
+			cout << endl;
+		}
 	}
 
 }
